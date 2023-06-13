467c066d1721715a25b622190074d47e1dab9687
fizaayesha <fizaayesha696@gmail.com>
2023-06-14 00:25:44 +0530

    event2

--- src/data/EventLists.js
+++ src/data/EventLists.js
@@ -1,5 +1,13 @@
 import event1 from "../Assets/event1.jpeg";
-export const UpcomingEventLists = [];
+import event1 from "../Assets/event1.jpg";
+export const UpcomingEventLists = [
+  {
+    image: event2,
+    title: "All about Internships",
+    desc: "Career Guidance Session | Path to SWE in product based Companies | Interview Prep Tips",
+    visit: "https://www.youtube.com/watch?v=tK4o2nHcSDY",
+  },
+];
 export const PastEventLists = [
   {
     image: event1,
